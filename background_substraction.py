6acf62fe184307c978b63e0568d6a7387e577c40
tstx <71097288+tstx@users.noreply.github.com>
2023-04-24 14:11:43 +0200

    added

---
+++ background_substraction.py
@@ -0,0 +1,58 @@
+from webcam import *
+import matplotlib.pyplot as plt
+import numpy as np
+
+# webcam_capture_loop(".")
+
+def colorConvert(image):
+  return(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+
+cam = Webcam()
+for i in range(30):
+  cam.get_latest_capture()
+
+
+# frame = cam.get_latest_capture()
+
+gray_frame_prev = cv2.cvtColor(cam.get_latest_capture(), cv2.COLOR_BGR2GRAY)
+
+
+#todo: blur before absdiff?
+frame_count = 0
+frame_max = 2000
+while (True):
+  frame_count += 1
+  # get image
+  frame = cam.get_latest_capture()
+
+  # Converting frame to grayscale
+  gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+  # Calculating Absolute Difference between Current Frame and Median Frame
+  dframe = cv2.absdiff(gray_frame, gray_frame_prev)
+  # dframe = cv2.dilate(dframe, np.ones((5, 5)), 1)
+  gray_frame_prev = gray_frame # todo remove and use median frame for diff (background)
+
+  # Applying Gaussian Blur to reduce noise
+  blur_frame = cv2.GaussianBlur(dframe, (11,11), 0)
+
+  # Binarizing frame - Thresholding
+  ret, threshold_frame = cv2.threshold(blur_frame, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+  # Identifying Contours
+  (contours, _ ) = cv2.findContours(threshold_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+  # Drawing Boundary Boxes for each Contour
+  # cv2.drawContours(image=frame, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
+  for i in contours:
+    x, y, width, height = cv2.boundingRect(i)
+    cv2.rectangle(frame, (x,y), (x + width, y + height), (123,0,255), 2)
+    
+  # Displaying the resulting frame
+  cv2.imshow('frame (press q to quit)', frame)
+  # Press 'q' to exit
+  if cv2.waitKey(1) & 0xFF == ord('q'):
+    break
+
+  # video_writer.write(cv2.resize(frame, (640,480)))
---
+++ webcam.py
@@ -0,0 +1,35 @@
+import cv2
+
+class Webcam:
+    def __init__(self):
+        self.cap = cv2.VideoCapture(0)
+        # Check if the webcam is opened correctly
+        if not self.cap.isOpened():
+            raise IOError("Cannot open webcam")
+        
+    def __del__(self):
+        self.cap.release()
+        cv2.destroyAllWindows()
+
+    def capture_loop(self, writepath, scale_factor = 1):
+        idx = 0
+        while True:
+            ret, frame = self.cap.read()
+            frame = cv2.resize(frame, None, fx=scale_factor, fy=scale_factor, interpolation=cv2.INTER_AREA)
+
+            cv2.imshow('Input', frame)
+            c = cv2.waitKey(1)
+            if c == 27: # 27 is the ASCII code for the ESC key
+                break
+            elif c == ord('r'):
+                #write frame to disk
+                filepath=f'{writepath}/{idx}.png'
+                print(f"recording... {filepath}")
+                cv2.imwrite(f'{filepath}', frame)
+                idx += 1
+        cv2.destroyAllWindows()
+
+
+    def get_latest_capture(self):
+        ret, frame = self.cap.read()
+        return frame
